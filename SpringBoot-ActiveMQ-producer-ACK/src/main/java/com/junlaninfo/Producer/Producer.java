24e57d6fde3c413898f62957d0c1a35a5e1ede19
xuexionghui <413669152@qq.com>
2019-12-05 16:04:43 +0800

    增添了手动ack 的内容

--- SpringBoot-ActiveMQ-producer-ACK/src/main/java/com/junlaninfo/Producer/Producer.java
+++ SpringBoot-ActiveMQ-producer-ACK/src/main/java/com/junlaninfo/Producer/Producer.java
@@ -1,34 +1,36 @@
-/**
- * 
- */
 package com.junlaninfo.Producer;
-
-import javax.jms.Queue;
-
+import javax.jms.Destination;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.scheduling.annotation.Schedules;
 import org.springframework.stereotype.Component;
-
-/**
- * @author 辉
- *
- */
+ 
 @Component
 @EnableScheduling
 public class Producer {
+	
 	@Autowired
 	private JmsTemplate jmsTemplate;
-	@Autowired
-	private  Queue  queue;
-	
+	/**
+	 * 发送消息，estination是发送到的队列，message是待发送的消息
+	 * @param destination
+	 * @param message
+	 */
+	/*
+	 * public void sendMessage(Destination destination, final String message) {
+	 * System.out.println(jmsTemplate.getDeliveryMode());
+	 * jmsTemplate.convertAndSend(destination, message); }
+	 */
+	/**
+	 * 发送消息，message是待发送的消息
+	 * @param message
+	 */
 	@Scheduled(fixedDelay = 5000)
-	public void  sendMessage() {
-		jmsTemplate.convertAndSend(queue,  
-				"测试消息队列" + System.currentTimeMillis());
+	public void sendMessage() {
+		System.out.println(jmsTemplate.getDeliveryMode());
+			jmsTemplate.convertAndSend("queue1","测试消息队列" + System.currentTimeMillis());
 	}
-
+ 
 }
--- SpringBoot-ActiveMQ-somsuner-ACK/src/main/java/com/junlaninfo/Consumer/Consumer.java
+++ SpringBoot-ActiveMQ-somsuner-ACK/src/main/java/com/junlaninfo/Consumer/Consumer.java
@@ -1,50 +1,28 @@
 package com.junlaninfo.Consumer;
-
-import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageListener;
-import javax.jms.Queue;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.listener.DefaultMessageListenerContainer;
 import org.springframework.stereotype.Component;
-
+ 
 @Component
 public class Consumer {
-	@Value("${queue}")
-	private String queue;
-
-	@Bean
-	public DefaultMessageListenerContainer defaultMessageListenerContainer(ConnectionFactory connectionFactory) {
-		DefaultMessageListenerContainer defaultMessageListenerContainer = new DefaultMessageListenerContainer();
-		defaultMessageListenerContainer.setSessionAcknowledgeMode(4);// 设置单条消息确认
-		defaultMessageListenerContainer.setDestinationName(queue);// 需要指定队列
-		defaultMessageListenerContainer.setSessionTransacted(false);// 关闭事物，否则不生效
-
-		MessageListener messageListener = new MessageListener() {
-
-			public void onMessage(Message message) {
-
-				try {
-					System.out.println("消费消息：" + message); // 手动确认消息，如若不确认，消息将一直存在于消息队列中
-					message.acknowledge();
-				} catch (JMSException e) {
-					e.printStackTrace();
-				}
-
-			}
-		}; // 消费消息
-		defaultMessageListenerContainer.setMessageListener(messageListener);
-
-		defaultMessageListenerContainer.setConnectionFactory(connectionFactory);
-		return defaultMessageListenerContainer;
-	}
-
-	// @JmsListener(destination = "${queue}")
-	public void receive(String msg) {
-		System.out.println("监听器收到msg:" + msg);
+ 
+	private final static Logger logger = LoggerFactory
+			.getLogger(Consumer.class);
+	
+	@JmsListener(destination = "queue1", containerFactory = "jmsQueueListener")
+	public void receiveQueue(final TextMessage text, Session session)
+			throws JMSException {
+		try {
+			System.out.println("Consumer收到的报文为:\" + "+text.getText());
+			logger.debug("Consumer收到的报文为:" + text.getText());
+			text.acknowledge();// 使用手动签收模式，需要手动的调用，如果不在catch中调用session.recover()消息只会在重启服务后重发
+		} catch (Exception e) {	
+			session.recover();// 此不可省略 重发信息使用
+		}
 	}
 }
